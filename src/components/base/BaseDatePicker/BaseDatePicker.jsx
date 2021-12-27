33b8ed72b3fde1849f5302d847b383328e939c3c
NIbragimov <nibragimov@NIbragimov-lt.1cb.kz>
2021-12-27 14:57:44 +0600

    refact BaseDatePicker

--- src/components/base/BaseDatePicker/BaseDatePicker.jsx
+++ src/components/base/BaseDatePicker/BaseDatePicker.jsx
@@ -5,13 +5,35 @@ import "react-datepicker/dist/react-datepicker.css"
 import styles from "./style.module.scss"
 
 const BaseDatePicker = ({date, width = 100, ...props}) => {
+    const months = [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+    ]
+    const weeks = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
     const CustomInput = React.forwardRef(({ value, onClick }, ref) => (
         <div
             style={{width: width + "%"}}
         >
             <input className={styles.datepicker__input} value={value} onClick={onClick} ref={ref} placeholder="дд/мм/гггг" readOnly/>
         </div>
-    ));
+    ))
+    const getMonth = (date) => {
+        const index = new Date(date).getMonth()
+        return months[index]
+    }
+    const getYear = (year) => {
+        return new Date(year).getFullYear()
+    }
 
     return (
         <>
@@ -21,6 +43,36 @@ const BaseDatePicker = ({date, width = 100, ...props}) => {
                 calendarClassName={styles.datepicker__calendar}
                 dayClassName={() => {
                     return styles.datepicker__day
+                }}
+                dateFormat="dd/MM/yyyy"
+                showMonthYearPicker={false}
+                locale={"Ru"}
+                renderCustomHeader={({date,
+                        changeYear,
+                        changeMonth,
+                        decreaseMonth,
+                        increaseMonth,
+                        prevMonthButtonDisabled,
+                        nextMonthButtonDisabled,
+                    }) => {
+                    return (
+                        <div>
+                            <div className={`${styles.datepicker__header} d-flex justify-content-between align-items-center`}>
+                                <div className={styles.datepicker__prev} onClick={decreaseMonth} disabled={prevMonthButtonDisabled}></div>
+                                <p>{getMonth(date)} {getYear(date)}</p>
+                                <div className={styles.datepicker__next} onClick={increaseMonth} disabled={nextMonthButtonDisabled}></div>
+                            </div>
+                            <div  className={`${styles.datepicker__weeks} d-flex justify-content-between align-items-center`}>
+                                {
+                                    weeks.map(week => {
+                                        return (
+                                            <div key={week}>{week}</div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    )
                 }}
                 {...props}
                 />
